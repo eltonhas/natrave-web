8f91e34030e733db912192ff8d636d27543deb10
Elton Hugo <elton_has@hotmail.com>
2022-10-28 13:29:38 -0300

    feat: fixed username to user on dashboard inthe right place

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -63,7 +63,7 @@ export const Dashboard = () => {
       <header className="bg-red-500 text-white p-4">
         <div className="container max-w-3xl flex justify-between">
           <Icon name="logoRed" className="w-28 md:w-40"/>
-          <a href={`${auth?.user?.name}`}>
+          <a href={`${auth?.user?.username}`}>
             <Icon name="profile" className="w-10"/>
           </a>
         </div>
@@ -72,7 +72,7 @@ export const Dashboard = () => {
       <main className="space-y-6">
         <section id="header" className="bg-red-500 text-white">
           <div className="container max-w-3xl space-y-2 p-4">
-            <span>Olá {`/${auth?.user?.username}`}</span>
+            <span>Olá {`/${auth?.user?.name}`}</span>
             <h3 className="text-2xl font-bold">Qual é o seu palpite?</h3>
           </div>
         </section>
